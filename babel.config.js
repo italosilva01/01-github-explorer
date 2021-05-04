fa93c2a0378fb565604f08c820e80880d4d5f336
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2021-05-04 19:56:27 -0300

    1#module finished

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
 };
 
 //"@babel/preset-react", serve para o babel traduzir o código react para o navegador
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-const { resolve } = require("path");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
   //arquivo principal da plicação
+  mode: isDevelopment ? "development" : "production",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   //saida do arquivo jsx convertido
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
@@ -13,13 +17,24 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   //regras de importação do webpack
   module: {
     //irei pegar todos os arquivos .jsx, menos os arquivos do node_modules e irei converter usando o babel-loader
-    rules: {
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      use: "babel-loader",
-    },
+    rules: [
+      { test: /\.jsx$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
